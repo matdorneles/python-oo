c17f6648cfe1b8f3dad57206990f97ba9a4b26dd
Matheus Dorneles <matheus.sdorneles1@gmail.com>
2022-04-12 16:46:21 -0300

    salvando projeto da aula

---
+++ conta.py
@@ -0,0 +1,37 @@
+
+
+
+class Conta:
+    def __init__(self, numero, titular, saldo, limite):
+        print("Construindo objeto")
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
+
+    def extrato(self):
+        print("Saldo de {} do titular {}".format(self.__saldo, self.__titular))
+
+    def depositar(self, valor):
+        self.__saldo += valor
+
+    def sacar(self, valor):
+        self.__saldo -= valor
+
+    def transferir(self, valor, destino):
+        self.sacar(valor)
+        destino.depositar(valor)
+
+    def get_saldo(self):
+        return self.__saldo
+
+    def get_titular(self):
+        return self.__titular
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
